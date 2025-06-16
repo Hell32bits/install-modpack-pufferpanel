fdd8ea31ce42f055ba67983097ebb6fbc57f5638
32BITS <100521632+Hell32bits@users.noreply.github.com>
2025-06-16 13:41:25 -0300

    Update install_modpack.sh

--- install_modpack.sh
+++ install_modpack.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Verifica se o link do modpack foi fornecido
-if [ -z "https://mediafilez.forgecdn.net/files/6222/83/Cursed%20Walking%201.20.1%20-%20Version%203.0.2%20SERVERPACK.zip" ]; then
-    echo "URL do modpack não fornecida, pulando instalação."
-    exit 0
+if [ -z "${modpack_url}" ]; then
+    echo "Erro: URL do modpack não fornecida."
+    exit 1
 fi
 
 # Nome do arquivo ZIP do modpack
@@ -12,8 +12,8 @@ MODPACK_ZIP="modpack.zip"
 # Diretório de destino para extração
 MODPACK_DIR="mods"
 
-echo "Baixando modpack de https://mediafilez.forgecdn.net/files/6222/83/Cursed%20Walking%201.20.1%20-%20Version%203.0.2%20SERVERPACK.zip..."
-wget -O "${MODPACK_ZIP}" "https://mediafilez.forgecdn.net/files/6222/83/Cursed%20Walking%201.20.1%20-%20Version%203.0.2%20SERVERPACK.zip"
+echo "Baixando modpack de ${modpack_url}..."
+wget -O "${MODPACK_ZIP}" "${modpack_url}"
 
 if [ $? -ne 0 ]; then
     echo "Erro ao baixar o modpack."
